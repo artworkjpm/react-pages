7ed9db20c01c4063ce3883a9ba044d257e86b33c
John Moran <johnnymoran44@hotmail.com>
2019-03-25 13:28:24 +0100

    Updates

---
+++ precache-manifest.66fb71c9b26875cc964aec53177d4c06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca2b9e796ab96ab77078",
+    "url": "/artworkjpm/react-pages/static/js/runtime~main.dbbf426a.js"
+  },
+  {
+    "revision": "32de400ca4f66d5ac256",
+    "url": "/artworkjpm/react-pages/static/js/main.ce45bcba.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/artworkjpm/react-pages/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "32de400ca4f66d5ac256",
+    "url": "/artworkjpm/react-pages/static/css/main.9f30b8b7.chunk.css"
+  },
+  {
+    "revision": "604c54a392a1fae699ec31b7cfe4120b",
+    "url": "/artworkjpm/react-pages/index.html"
+  }
+];
